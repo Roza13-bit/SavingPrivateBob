2f996ca02246e428067811752c9a4071f433f9eb
Roza13-bit <roizamir876@gmail.com>
2020-12-03 22:14:14 +0200

    V0.8 - New Assets, Player, Camera
    * Added new assets.
    * Map was arranged is a basic layout.
    * Player was added + movement +
      animation.
    * Camera was put in the back of the
      player.

---
+++ Assets/Scripts/MyGuyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGuyController : MonoBehaviour
+{
+    [SerializeField] CharacterController myGuyCC;
+
+    [SerializeField] float movementSpeed = 5;
+
+    [SerializeField] float smoothTurning = 0.1f;
+
+    [SerializeField] Animator animator;
+
+    float turnSmoothVelocity;
+
+    Vector3 moveVector;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = transform.right * horizontal + transform.forward * vertical;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTurning);
+
+            animator.SetBool("isRunning", true);
+
+            myGuyCC.Move(direction * movementSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+
+        }
+
+        moveVector = Vector3.zero;
+
+        if (myGuyCC.isGrounded == false)
+        {
+            moveVector += Physics.gravity;
+
+        }
+
+        myGuyCC.Move(moveVector * Time.deltaTime);
+
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+
+        } */
+
+    }
+
+}
